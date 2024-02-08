80e61956b5c73578acb0f7d3c7f5f98ecb320a84
Nitin Naikwade <nitinnaikwade53@gmail.com>
2024-02-08 17:46:55 +0000

    Add tf state bucket

---
+++ resources/gcs_tf_bucket/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "5.15.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+}   
+
+resource "google_storage_bucket" "default" {
+  name          = "nitin-bucket-tfstate"
+  force_destroy = false
+  location      = "US"
+  storage_class = "STANDARD"
+  versioning {
+    enabled = true
+  }
+  encryption {
+    default_kms_key_name = google_kms_crypto_key.terraform_state_bucket.id
+  }
+  depends_on = [
+    google_project_iam_member.default
+  ]
+}
